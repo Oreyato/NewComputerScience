7f63177d9a2976ec4dcb7c4b03584ce4bbf2c723
Oreyato <oreyato.pro@gmail.com>
2022-10-24 20:39:17 +0200

    com: last commit before trying to remove the vectors output

--- ComputerScience/ComputerScience/main.cpp
+++ ComputerScience/ComputerScience/main.cpp
@@ -31,14 +31,16 @@ int main() {
 	// Initialise randomness seed
 	srand((unsigned)time(0));
 	// Create the vector to sort
-	vector<float> floatVector;
+	//vector<float> floatVector;
 	// Initialise the vector's max size
 	int maxSize = 5;
 	// Initialise the value's range
 	float range = 50.0f;
 	// And initialise it
-	initVector(floatVector, maxSize, -range, range);
+	//initVector(floatVector, maxSize, -range, range);
 	 
+	vector<float> floatVector = {36, 19, -25, 49, -4};
+
 	cout << "Initial vector:" << endl;
 	// Print initial vector
 	for (int i = 0; i < maxSize; i++)
